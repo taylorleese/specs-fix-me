7cb57741b05a67fea69cdac824be6d1aa906a749
Taylor Leese <taylor@stackmob.com>
2012-03-12 15:28:31 -0700

    add a few tests

--- src/test/scala/com/specsfixme/tests/CoinSpecs.scala
+++ src/test/scala/com/specsfixme/tests/CoinSpecs.scala
@@ -14,8 +14,10 @@ class CoinSpecs extends Specification { def is =
     "Denominations: [1], Value: 12, Expected: 12"                               ! coins(List(1), 12, 12).find ^
     "Denominations: [1, 2, 3], Value: 6, Expected: 2"                           ! coins(List(1, 2, 3), 6, 2).find ^
     "Denominations: [1, 3, 4], Value: 6, Expected: 2"                           ! coins(List(1, 3, 4), 6, 2).find ^
+    "Denominations: [4, 3, 1], Value: 6, Expected: 2"                           ! coins(List(4, 3, 1), 6, 2).find ^
     "Denominations: [1, 5, 10, 25], Value: 41, Expected: 4"                     ! coins(List(1, 5, 10, 25), 41, 4).find ^
     "Denominations: [1, 4, 5, 15], Value: 8, Expected: 2"                       ! coins(List(1, 4, 5, 15), 8, 2).find ^
+    "Denominations: [15, 1, 5, 4], Value: 8, Expected: 2"                       ! coins(List(15, 1, 5, 4), 8, 2).find ^
                                                                                 endp^
                                                                                 end
   
